65ef0e0b1aa025a02c498c7e255d9dfa3f896746
Jae Lim <jaelim@visa.com>
2016-07-12 10:56:05 -0500

    Finished enumeration project

--- Enum/src/com/casinomachine/Application.java
+++ Enum/src/com/casinomachine/Application.java
@@ -1,5 +1,9 @@
 package com.casinomachine;
 
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Application {
 
 	public enum MachineStates {
@@ -7,7 +11,25 @@ public class Application {
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ThreadLocalRandom tlr = ThreadLocalRandom.current();
+		
+		Iterator<Integer> iter = tlr.ints(0, 6).iterator();
+		int count = 0;
+		StringBuilder sb = new StringBuilder();
+		String[] results = new String[3];
+		while(iter.hasNext() && count < 9){
+			sb.append(MachineStates.values()[iter.next()]);
+			count ++;
+			if(!(count % 3 == 0)){
+				sb.append(" : ");
+			}
+			if (count % 3 == 0){
+				results[count/3 - 1] = sb.toString();
+				sb = new StringBuilder();
+			}
+		}
+		
+		System.out.println(Arrays.toString(results));
 
 	}
 
