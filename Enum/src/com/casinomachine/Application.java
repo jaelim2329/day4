d9287407148a08f6b6d0a1616bb7b2941d043f94
Jae Lim <jaelim@visa.com>
2016-07-12 10:36:21 -0500

    Enumeration lab

---
+++ Enum/src/com/casinomachine/Application.java
@@ -0,0 +1,14 @@
+package com.casinomachine;
+
+public class Application {
+
+	public enum MachineStates {
+		BAR, MEDAL, COIN, TRIPLEBAR, CHERRY, JACKPOT;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
